9c7defd1508681d69a894793c06b27b50c13ea97
rusty <shaun@afb.com>
2015-10-23 09:41:38 +0200

    Update reverse order of events

--- DecisionTask.py
+++ DecisionTask.py
@@ -11,8 +11,10 @@ if __name__ == '__main__':
         try:
             print('Polling for decision ..')
             task = conn.poll_for_decision_task(domain='SWF-Prototype', taskList={'name': 'PrintConsoleMessageTask'},
-                                            identity='shaun-2')
+                                            identity='shaun-2', reverseOrder=True)
 
+
+            # for n = task[''] -- loop through the tasks
             print('Decision: ', task)
             conn.respond_decision_task_completed(
                 taskToken=task['taskToken'],
@@ -22,9 +24,13 @@ if __name__ == '__main__':
                         'scheduleActivityTaskDecisionAttributes': {
                             'activityType':{
                                 'name': 'PrintConsoleMessage',
-                                'version': '1.0'
+                                'version': '1.1'
                                 },
-                            'activityId': 'fee78257-c71d-485f-b658-35f666bc20ad'
+                            'activityId': 'fee78257-c71d-485f-b658-35f666bc20ad',
+                            'scheduleToCloseTimeout': 'NONE',
+                            'scheduleToStartTimeout': 'NONE',
+                            'startToCloseTimeout': 'NONE',
+                            'heartbeatTimeout': 'NONE'
                         }
                     }
                 ]
